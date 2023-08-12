c9eb91c7f011f3053b79ee9beb99a62a4f3817b9
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2023-08-12 13:18:42 -0300

    Added some commentaries and bug fixes to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,86 +1,77 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/// Define a structure to hold student information
+struct cadastro_de_aluno
+{
+    char nome[50];
+    char disciplina[50];
+    float nota_prova1;
+    float nota_prova2;
+    char matricula[10];
+    char email[50];
+    float telefone;
+};
 
-int main() {
-	struct cadastro_de_aluno {
-	char nome[50];
-	char disciplina [50];
-	float nota_prova1 ;
-	float nota_prova2 ;
-	char matricula[10];
-	char email[50];
-	float telefone;
-	};
-	struct professor{
-	
-	char nome[50];
-	char matricula;
-	char cpf[11];
-	char endereco [90];
-	};
+/// Define a structure to hold professor information
+struct professor
+{
+    char nome[50];
+    char matricula[10];
+    char cpf[12];
+    char endereco[90];
+};
 
-	struct cadastro_de_aluno aluno;
-	struct professor;
-	
-	printf("\n\n---------------Cadastro de Professor---------------\n\n");
-	printf("Nome:\n");
-	fflush(stdin);
-	fgets(professor.nome,50,stdin);
-	
-	printf("Matrícula:\n");
-	fflush(stdin);
-	fgets(professor.matricula,10,stdin);
-	
-	printf("CPF:\n");
-	fflush(stdin);
-	fgets(professor.cpf,11,stdin);
-	
-	printf("Endereço:\n");
-	fflush(stdin);
-	fgets(professor.endereco,90,stdin);
-	
-	printf("\n\n---------------Cadastro de Aluno---------------\n\n");
-	printf("Nome:\n");
-	fflush(stdin);
-	fgets(aluno.nome,50,stdin);
-	
-	printf("Matrícula:\n");
-	fflush(stdin);
-	fgets(aluno.matricula,10,stdin);
+int main()
+{
+    struct cadastro_de_aluno aluno;
+    struct professor professor;
 
-	printf("Email:\n");
-	fflush(stdin);
-	fgets(aluno.email,50,stdin);
-	
-	printf("Telefone:\n");
-	scanf("%f",&aluno.telefone);
+    /// Input for professor's information
+    printf("\n\n---------------Cadastro de Professor---------------\n\n");
+    printf("Nome:\n");
+    fgets(professor.nome, 50, stdin);
+    printf("Matricula:\n");
+    fgets(professor.matricula, 10, stdin);
+    printf("CPF:\n");
+    fgets(professor.cpf, 12, stdin);
+    printf("Endereco:\n");
+    fgets(professor.endereco, 90, stdin);
 
-	
-	printf("Disciplina:\n");
-	fflush(stdin);
-	fgets(aluno.disciplina,50,stdin);
-	
-	printf("Informe a 1a. nota..:\n");
-	scanf("%f",&aluno.nota_prova1);
-	
-	
-	printf("Informe a 2a. nota..:\n");
-	scanf("%f", &aluno.nota_prova2);
-	
-	
-	printf("\n\n---------------Lendo os dados da struct...---------------\n\n");
-	printf("Nome: %s",aluno.nome);
-	printf("Disciplina: %s",aluno.disciplina);
-	printf("Nota prova 1: %.2f\n",aluno.nota_prova1);
-	printf("Nota prova 2: %.2f\n",aluno.nota_prova2);
-	printf("Matrícula %.2f\n",aluno.matricula);
-	printf("email: %.2f\n",aluno.email);
-	printf("Telefone %.2f\n",aluno.telefone);
+    /// Input for student's information
+    printf("\n\n---------------Cadastro de Aluno---------------\n\n");
+    printf("Nome:\n");
+    fgets(aluno.nome, 50, stdin);
+    printf("Matricula:\n");
+    fgets(aluno.matricula, 10, stdin);
+    printf("Email:\n");
+    fgets(aluno.email, 50, stdin);
+    printf("Telefone:\n");
+    scanf("%f", &aluno.telefone);
+    while (getchar() != '\n'); // Clear input buffer
+    printf("Disciplina:\n");
+    fgets(aluno.disciplina, 50, stdin);
+    printf("Informe a 1a. nota:\n");
+    scanf("%f", &aluno.nota_prova1);
+    printf("Informe a 2a. nota:\n");
+    scanf("%f", &aluno.nota_prova2);
 
-	
-		return 0;
-	};
+    /// Displaying student's and professor's information
+    printf("\n\n---------------Lendo os dados da struct Aluno---------------\n\n");
+    printf("Nome do aluno: %s", aluno.nome);
+    printf("Disciplina: %s", aluno.disciplina);
+    printf("Nota prova 1: %.2f\n", aluno.nota_prova1);
+    printf("Nota prova 2: %.2f\n", aluno.nota_prova2);
+    printf("Matricula do aluno: %s\n", aluno.matricula);
+    printf("Email do aluno: %s\n", aluno.email);
+    printf("Telefone do aluno: %.2f\n", aluno.telefone);
 
+    printf("\n\n---------------Lendo os dados da struct Professor---------------\n\n");
+    printf("Nome do Professor: %s\n",professor.nome);
+    printf("CPF:%s\n",professor.cpf);
+    printf("Matricula:%s\n",professor.matricula);
+    printf("Endereco:%s\n",professor.endereco);
 
+    return 0;
+}
